a0d9a8c8ad2f0e4f2483de290067f30f553e4151
runninbear5 <blakelieber@gmail.com>
2018-07-18 11:57:04 -0700

    Adding touch screen compadability

--- sketch.js
+++ sketch.js
@@ -5,13 +5,17 @@ var longRect = 650;
 var won = false;
 var lost = false;
 var birdCreate;
+var prevTouch;
 
 function setup(){
 	createCanvas(650,650);
-	addBoxes(2);	
+	addBoxes(2);
 }
 
 function draw(){
+	if(touches.length > 0){
+			prevTouch = createVector(touches[0].x, touches[0].y);
+	}
 	if(!won){
 		background(204, 190, 178);
 		rectMode(CORNER);
@@ -49,6 +53,18 @@ function draw(){
 	}
 }
 
+function touchMoved(){
+	if(touches[0].x > prevTouch.x){
+		moveRight();
+	}else if(touches[0].x < prevTouch.x){
+		moveLeft();
+	}else if(touches[0].y > prevTouch.y){
+		moveUp();
+	}else if(touches[0].y < prevTouch.y){
+		moveDown();
+	}
+}
+
 function addBoxes(num){
 	for(var i=0; i<num; i++){
 		var newBox = new Box(locations[(int)(Math.random()*4)], locations[(int)(Math.random()*4)]);
@@ -70,157 +86,13 @@ function addBoxes(num){
 function keyPressed(){
 	if(!lost){
 		if(keyCode === RIGHT_ARROW){
-			var boxsMoved = 0;
-			var startingBoxLength = boxs.length;
-			while(boxsMoved < startingBoxLength){
-				var rightMostIdx = -1;
-				var rightMostPos = 0;
-				for(var i=0; i<boxs.length; i++){
-					if( !boxs[i].tryMove && boxs[i].x > rightMostPos){
-						rightMostPos = boxs[i].x;
-						rightMostIdx = i;
-					}
-				}
-				if(rightMostIdx != -1){
-					var box = boxs[rightMostIdx];
-					var locIdx = locations.indexOf(box.x);
-					var collision = false;
-					var moveAmount = 0;
-					while(locIdx+1<locations.length && !collision){
-						for(var i=0; i<boxs.length; i++){
-							if(boxs[i].x === locations[locIdx+1] && boxs[i].y === box.y){
-								collision = true;
-								if(boxs[i].value === box.value){
-									boxs[i].increaseValue();
-									boxs.splice(rightMostIdx, 1);
-									box.movedSuc = true;
-								}
-							}
-						}
-						if(!collision){
-							locIdx = locIdx+1;
-							box.x = locations[locIdx];
-							box.movedSuc = true;
-						}
-					}
-					boxsMoved ++;
-					box.tryMove = true;
-				}
-			}
+			moveRight();
 		}else if(keyCode === LEFT_ARROW){
-			var boxsMoved = 0;
-			var startingBoxLength = boxs.length;
-			while(boxsMoved < startingBoxLength){
-				var leftMostIdx = -1;
-				var leftMostPos = 580;
-				for(var i=boxs.length-1; i>-1; i--){
-					if( !boxs[i].tryMove && boxs[i].x < leftMostPos){
-						leftMostPos = boxs[i].x;
-						leftMostIdx = i;
-					}
-				}
-				if(leftMostIdx != -1){
-					var box = boxs[leftMostIdx];
-					var locIdx = locations.indexOf(box.x);
-					var collision = false;
-					var moveAmount = 0;
-					while(locIdx-1>-1 && !collision){
-						for(var i=0; i<boxs.length; i++){
-							if(boxs[i].x === locations[locIdx-1] && boxs[i].y === box.y){
-								collision = true;
-								if(boxs[i].value === box.value){
-									boxs[i].increaseValue();
-									boxs.splice(leftMostIdx, 1);
-									box.movedSuc = true;
-								}
-							}
-						}
-						if(!collision){
-							locIdx = locIdx-1;
-							box.x = locations[locIdx];
-							box.movedSuc = true;
-						}
-					}
-					boxsMoved ++;
-					box.tryMove = true;
-				}
-			}
+			moveLeft();
 		}else if(keyCode === UP_ARROW){
-			var boxsMoved = 0;
-			var startingBoxLength = boxs.length;
-			while(boxsMoved < startingBoxLength){
-				var upMostIdx = -1;
-				var upMostPos = 580;
-				for(var i=boxs.length-1; i>-1; i--){
-					if( !boxs[i].tryMove && boxs[i].y < upMostPos){
-						upMostPos = boxs[i].y;
-						upMostIdx = i;
-					}
-				}
-				if(upMostIdx != -1){
-					var box = boxs[upMostIdx];
-					var locIdx = locations.indexOf(box.y);
-					var collision = false;
-					var moveAmount = 0;
-					while(locIdx-1>-1 && !collision){
-						for(var i=0; i<boxs.length; i++){
-							if(boxs[i].y === locations[locIdx-1] && boxs[i].x === box.x){
-								collision = true;
-								if(boxs[i].value === box.value){
-									boxs[i].increaseValue();
-									boxs.splice(upMostIdx, 1);
-									box.movedSuc = true;
-								}
-							}
-						}
-						if(!collision){
-							locIdx = locIdx-1;
-							box.y = locations[locIdx];
-							box.movedSuc = true;
-						}
-					}
-					boxsMoved ++;
-					box.tryMove = true;
-				}
-			}
+			moveUp();
 		}else if(keyCode === DOWN_ARROW){
-			var boxsMoved = 0;
-			var startingBoxLength = boxs.length;
-			while(boxsMoved < startingBoxLength){
-				var downMostIdx = -1;
-				var downMostPos = 0;
-				for(var i=0; i<boxs.length; i++){
-					if( !boxs[i].tryMove && boxs[i].y > downMostPos){
-						downMostPos = boxs[i].y;
-						downMostIdx = i;
-					}
-				}
-				if(downMostIdx != -1){
-					var box = boxs[downMostIdx];
-					var locIdx = locations.indexOf(box.y);
-					var collision = false;
-					var moveAmount = 0;
-					while(locIdx+1<locations.length && !collision){
-						for(var i=0; i<boxs.length; i++){
-							if(boxs[i].y === locations[locIdx+1] && boxs[i].x === box.x){
-								collision = true;
-								if(boxs[i].value === box.value){
-									boxs[i].increaseValue();
-									boxs.splice(downMostIdx, 1);
-									box.movedSuc = true;
-								}
-							}
-						}
-						if(!collision){
-							locIdx = locIdx+1;
-							box.y = locations[locIdx];
-							box.movedSuc = true;
-						}
-					}
-					boxsMoved ++;
-					box.tryMove = true;
-				}
-			}
+			moveDown();
 		}
 		var boxMove = false;
 		for(var i=0; i<boxs.length; i++){
@@ -235,6 +107,166 @@ function keyPressed(){
 		}
 		if(boxs.length === 16){
 				lost = true;
+		}
+	}
+}
+
+function moveRight(){
+	var boxsMoved = 0;
+	var startingBoxLength = boxs.length;
+	while(boxsMoved < startingBoxLength){
+		var rightMostIdx = -1;
+		var rightMostPos = 0;
+		for(var i=0; i<boxs.length; i++){
+			if( !boxs[i].tryMove && boxs[i].x > rightMostPos){
+				rightMostPos = boxs[i].x;
+				rightMostIdx = i;
+			}
+		}
+		if(rightMostIdx != -1){
+			var box = boxs[rightMostIdx];
+			var locIdx = locations.indexOf(box.x);
+			var collision = false;
+			var moveAmount = 0;
+			while(locIdx+1<locations.length && !collision){
+				for(var i=0; i<boxs.length; i++){
+					if(boxs[i].x === locations[locIdx+1] && boxs[i].y === box.y){
+						collision = true;
+						if(boxs[i].value === box.value){
+							boxs[i].increaseValue();
+							boxs.splice(rightMostIdx, 1);
+							box.movedSuc = true;
+						}
+					}
+				}
+				if(!collision){
+					locIdx = locIdx+1;
+					box.x = locations[locIdx];
+					box.movedSuc = true;
+				}
+			}
+			boxsMoved ++;
+			box.tryMove = true;
+		}
+	}
+}
+
+function moveLeft(){
+	var boxsMoved = 0;
+	var startingBoxLength = boxs.length;
+	while(boxsMoved < startingBoxLength){
+		var leftMostIdx = -1;
+		var leftMostPos = 580;
+		for(var i=boxs.length-1; i>-1; i--){
+			if( !boxs[i].tryMove && boxs[i].x < leftMostPos){
+				leftMostPos = boxs[i].x;
+				leftMostIdx = i;
+			}
+		}
+		if(leftMostIdx != -1){
+			var box = boxs[leftMostIdx];
+			var locIdx = locations.indexOf(box.x);
+			var collision = false;
+			var moveAmount = 0;
+			while(locIdx-1>-1 && !collision){
+				for(var i=0; i<boxs.length; i++){
+					if(boxs[i].x === locations[locIdx-1] && boxs[i].y === box.y){
+						collision = true;
+						if(boxs[i].value === box.value){
+							boxs[i].increaseValue();
+							boxs.splice(leftMostIdx, 1);
+							box.movedSuc = true;
+						}
+					}
+				}
+				if(!collision){
+					locIdx = locIdx-1;
+					box.x = locations[locIdx];
+					box.movedSuc = true;
+				}
+			}
+			boxsMoved ++;
+			box.tryMove = true;
+		}
+	}
+}
+
+function moveUp(){
+	var boxsMoved = 0;
+	var startingBoxLength = boxs.length;
+	while(boxsMoved < startingBoxLength){
+		var upMostIdx = -1;
+		var upMostPos = 580;
+		for(var i=boxs.length-1; i>-1; i--){
+			if( !boxs[i].tryMove && boxs[i].y < upMostPos){
+				upMostPos = boxs[i].y;
+				upMostIdx = i;
+			}
+		}
+		if(upMostIdx != -1){
+			var box = boxs[upMostIdx];
+			var locIdx = locations.indexOf(box.y);
+			var collision = false;
+			var moveAmount = 0;
+			while(locIdx-1>-1 && !collision){
+				for(var i=0; i<boxs.length; i++){
+					if(boxs[i].y === locations[locIdx-1] && boxs[i].x === box.x){
+						collision = true;
+						if(boxs[i].value === box.value){
+							boxs[i].increaseValue();
+							boxs.splice(upMostIdx, 1);
+							box.movedSuc = true;
+						}
+					}
+				}
+				if(!collision){
+					locIdx = locIdx-1;
+					box.y = locations[locIdx];
+					box.movedSuc = true;
+				}
+			}
+			boxsMoved ++;
+			box.tryMove = true;
+		}
+	}
+}
+
+function moveDown(){
+	var boxsMoved = 0;
+	var startingBoxLength = boxs.length;
+	while(boxsMoved < startingBoxLength){
+		var downMostIdx = -1;
+		var downMostPos = 0;
+		for(var i=0; i<boxs.length; i++){
+			if( !boxs[i].tryMove && boxs[i].y > downMostPos){
+				downMostPos = boxs[i].y;
+				downMostIdx = i;
+			}
+		}
+		if(downMostIdx != -1){
+			var box = boxs[downMostIdx];
+			var locIdx = locations.indexOf(box.y);
+			var collision = false;
+			var moveAmount = 0;
+			while(locIdx+1<locations.length && !collision){
+				for(var i=0; i<boxs.length; i++){
+					if(boxs[i].y === locations[locIdx+1] && boxs[i].x === box.x){
+						collision = true;
+						if(boxs[i].value === box.value){
+							boxs[i].increaseValue();
+							boxs.splice(downMostIdx, 1);
+							box.movedSuc = true;
+						}
+					}
+				}
+				if(!collision){
+					locIdx = locIdx+1;
+					box.y = locations[locIdx];
+					box.movedSuc = true;
+				}
+			}
+			boxsMoved ++;
+			box.tryMove = true;
 		}
 	}
 }
